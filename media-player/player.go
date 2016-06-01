329d511a8620d9fb7b98254fbd743105940ffc31
awwalker <awwalker3@gmail.com>
2016-06-01 10:53:24 -0700

    real fix

--- media-player/player.go
+++ media-player/player.go
@@ -141,7 +141,7 @@ func (api mediaHandlers) ListObjectsHandler(w http.ResponseWriter, r *http.Reque
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			playListEntry.URL = presignedURL.String()
+			playListEntry.URL = String(presignedURL)
 		}
 		playListEntries = append(playListEntries, playListEntry)
 	}
@@ -168,5 +168,5 @@ func (api mediaHandlers) GetPresignedURLHandler(w http.ResponseWriter, r *http.R
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(presignedURL.String()))
+	w.Write([]byte(String(presignedURL)))
 }
